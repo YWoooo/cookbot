81e65497bf0e9283303702fda9e7a5d63df3546a
YuWeiChang <ywchangsmail@gmail.com>
2020-01-08 11:06:10 +0800

    Add function to deal with empty query and empty result.

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const normalMsg = require('./src/language/normalMsg')
 
 async function sendPage(context) {
     const page = await cookbook.getPage(context.event.text)
+
+    if (Object.keys(page).length === 0) {
+        return foundNothing
+    }
+
     await context.sendText(page[0].title);
     await context.sendText(`  · ${page[0].summary[0]}`)
 }
 
-async function sendUnknown(context) {
-    await context.sendText(normalMsg.en.foundNothing);
+function foundNothing(context) {
+    context.sendText(normalMsg.en.foundNothing);
+}
+
+function emptyQuery(context) {
+    context.sendText(normalMsg.en.emptyQuery)
 }
 
 module.exports = async function App(context) {
-    if (context.event.text.trim().length === 0) {
-        return await context.sendText(normalMsg.en.emptyQuery)
-    }
-    //return context.event.text === 'custom' ? sendPage : sendUnknown;
-    return sendPage
+    return context.event.text.trim().length === 0 ? emptyQuery : sendPage
 };
