37f78f6095d496527fe3d844dc071a3cc730edf8
YuWeiChang <ywchangsmail@gmail.com>
2020-01-09 12:54:17 +0800

    Some basic setting for messenger.

--- bottender.config.js
+++ bottender.config.js
@@ -1,10 +1,23 @@
+require('dotenv').config({ path: './process.env' })
+
 module.exports = {
-    session: {
-      driver: 'memory',
-      stores: {
-        memory: {
-          maxSize: 500,
-        },
+  session: {
+    driver: 'memory',
+    stores: {
+      memory: {
+        maxSize: 500,
       },
     },
-  };
+  },
+  channels: {
+    messenger: {
+      enabled: true,
+      path: '/',
+      pageId: process.env.MESSENGER_PAGE_ID,
+      accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+      verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+      appId: process.env.MESSENGER_APP_ID,
+      appSecret: process.env.MESSENGER_APP_SECRET,
+    },
+  },
+};
