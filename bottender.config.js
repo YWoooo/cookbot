6d097620dae6d46ee9bd25454066d64eb08cab93
YuWeiChang <ywchangsmail@gmail.com>
2020-01-13 18:47:52 +0800

    Welcome message.

--- bottender.config.js
+++ bottender.config.js
@@ -18,6 +18,14 @@ module.exports = {
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
+      profile: {
+        greeting: [
+          {
+            locale: 'default',
+            text: "Hello, {{user_first_name}}, I'm your cookbot. Just give me any text and I'll find if there is a match in the cookbook!",
+          }
+        ],
+      }
     },
   },
 };
